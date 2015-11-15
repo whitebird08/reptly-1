4d7e827ef7e59d81286b5f039ec41c0cb648639a
Ryan Bird <ryan01bird@gmail.com>
2015-11-15 12:20:49 -0700

    added about routes

--- app.js
+++ app.js
@@ -9,5 +9,17 @@ angular.module('reptlyApp', ['ngRoute'])
 				templateUrl: '/partials/home.html',
 				controller: 'HomeController'
 			})
+			.when('/aboutSnakes', {
+				templateUrl: '/partials/aboutSnakes.html',
+				controller: 'HomeController'
+			})
+			.when('/aboutLizards', {
+				templateUrl: '/partials/aboutLizards.html',
+				controller: 'HomeController'
+			})
+			.when('/aboutTurtles', {
+				templateUrl: '/partials/aboutTurtles.html',
+				controller: 'HomeController'
+			})
 		$locationProvider.html5Mode(true);
 	})
